2b4595ae5d87a6ff993c90f1dfed57f25a7fc541
tawannantawat <tawanswc@gmail.com>
2022-10-06 15:12:25 +0700

    register + login

--- pages/api/user/register.js
+++ pages/api/user/register.js
@@ -9,7 +9,12 @@ export default function userRegisterRoute(req, res) {
     //check authentication
     const user = checkToken(req);
     //return res.status(403).json({ok: false,message: "You do not have permission to create account",});
-
+    if (!user || user.isAdmin === false) {
+      return res.status(403).json({
+        ok: false,
+        message: "You do not have permission to create account",
+      });
+    }
     //validate body
     if (
       typeof username !== "string" ||
@@ -25,11 +30,26 @@ export default function userRegisterRoute(req, res) {
     //check if username is already in database
     const users = readUsersDB();
     //return res.status(400).json({ ok: false, message: "Username is already taken" });
+    const foundUsername = users.find((x) => username === x.username);
+    if (foundUsername) {
+      return res
+        .status(400)
+        .json({ ok: false, message: "Username is already taken" });
+    }
 
     //create new user and add in db
+    const newUser = {
+      username: username,
+      password: bcrypt.hashSync(password, 12),
+      isAdmin: isAdmin,
+      money: isAdmin ? null : 0,
+    };
+
+    users.push(newUser);
 
     writeUsersDB(users);
 
     //return response
+    res.json({ ok: true, username, isAdmin });
   }
 }
